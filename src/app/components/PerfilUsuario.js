9425ce1eaa5f82d226b6e5300c3bd96319221592
SANTIAGO QUINTERO <you@example.com>
2019-09-18 06:27:26 -0500

    PerfilUsuario integrado

--- src/app/components/PerfilUsuario.js
+++ src/app/components/PerfilUsuario.js
@@ -16,70 +16,70 @@ class PerfilUsuario extends Component {
                         de
                         hacer turismo responsable</p>
                 </div>
-            <div className="container">
-                <form className="text-primary">
-                    <div className="form-row">
-                        <div className="col-md-4 mb-3">
-                            <label for="validationDefault03">Nombre</label>
-                            <input type="text" className="form-control"
-                                id="validationDefault02" placeholder="Nombre"
-                                required></input>
+                <div className="container">
+                    <form className="text-primary">
+                        <div className="form-row">
+                            <div className="col-md-4 mb-3">
+                                <label for="validationDefault03">Nombre</label>
+                                <input type="text" className="form-control"
+                                    id="validationDefault02" placeholder="Nombre"
+                                    required></input>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <label for="validationDefault02">Apellidos</label>
+                                <input type="text" className="form-control"
+                                    id="validationDefault02" placeholder="Apellidos"
+                                    required></input>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <label for="validationDefaultUsername">Correo
+                                    electrónico</label>
+                                <input type="email" className="form-control"
+                                    id="validationDefault02" placeholder="Correo"
+                                    required></input>
+                            </div>
                         </div>
-                        <div className="col-md-4 mb-3">
-                            <label for="validationDefault02">Apellidos</label>
-                            <input type="text" className="form-control"
-                                id="validationDefault02" placeholder="Apellidos"
-                                required></input>
+                        <div className="form-row">
+                            <div className="col-md-4 mb-3">
+                                <label for="validationDefault03">Tipo de documento</label>
+                                <select className="custom-select">
+                                    <option selected></option>
+                                </select>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <label for="validationDefault02">Número de documento</label>
+                                <input type="number" className="form-control"
+                                    id="validationDefault02" placeholder="Número de
+                                    documento"
+                                    required></input>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <label for="validationDefault05">Teléfono</label>
+                                <input type="number" className="form-control"
+                                    id="validationDefault05" placeholder="Teléfono"
+                                    required></input>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <label for="validationDefault05">Confirmar
+                                    contraseña</label>
+                                <input type="password" className="form-control"
+                                    id="validationDefault05" placeholder="******"
+                                    required></input>
+                            </div>
                         </div>
-                        <div className="col-md-4 mb-3">
-                            <label for="validationDefaultUsername">Correo
-                                electrónico</label>
-                            <input type="email" className="form-control"
-                                id="validationDefault02" placeholder="Correo"
-                                required></input>
+                        <div className="form-group">
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"
+                                    id="invalidCheck2" required></input>
+                                <label className="form-check-label" for="invalidCheck2">
+                                    Acepto los términos y condiciones
+                                </label>
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="col-md-4 mb-3">
-                            <label for="validationDefault03">Tipo de documento</label>
-                            <select className="custom-select">
-                                <option selected></option>
-                            </select>
-                        </div>
-                        <div className="col-md-4 mb-3">
-                            <label for="validationDefault02">Número de documento</label>
-                            <input type="number" className="form-control"
-                                id="validationDefault02" placeholder="Número de
-                                documento"
-                                required></input>
-                        </div>
-                        <div className="col-md-4 mb-3">
-                            <label for="validationDefault05">Teléfono</label>
-                            <input type="number" className="form-control"
-                                id="validationDefault05" placeholder="Teléfono"
-                                required></input>
-                        </div>
-                        <div className="col-md-4 mb-3">
-                            <label for="validationDefault05">Confirmar
-                                contraseña</label>
-                            <input type="password" className="form-control"
-                                id="validationDefault05" placeholder="******"
-                                required></input>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-check">
-                            <input className="form-check-input" type="checkbox"
-                                id="invalidCheck2" required></input>
-                            <label className="form-check-label" for="invalidCheck2">
-                                Acepto los términos y condiciones
-                            </label>
-                        </div>
-                    </div>
-                    <button className="btn btn-primary" type="submit">Actualizar
+                        <button className="btn btn-primary" type="submit">Actualizar
                         Perfil</button>
-            </form>
-            </div>
+                    </form>
+                </div>
             </div>
         );
     }
