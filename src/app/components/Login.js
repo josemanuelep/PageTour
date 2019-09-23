300f2526ac0f88f8f2a3a1eccceed0144188359d
SANTIAGO QUINTERO <you@example.com>
2019-09-22 22:00:33 -0500

    Login Frontend parcial

---
+++ src/app/components/Login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <main className="main">
+
+                    <form>
+                        <div className="form-group">
+                            <label for="exampleInputEmail1">Correo electrónico</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Correo" required></input>
+                            <small id="emailHelp" className="form-text text-muted">Nunca compartiremos su correo electrónico con nadie más.</small>
+                        </div>
+                        <div className="form-group">
+                            <label for="exampleInputPassword1">Contraseña</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Contraseña"></input>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"></input>
+                            <label className="form-check-label" for="inlineRadio1">Entidad Oficial</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"></input>
+                            <label className="form-check-label" for="inlineRadio2">Empresa Propietaria</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"></input>
+                            <label className="form-check-label" for="inlineRadio3">Turista</label>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Ingresar</button>
+                    </form>
+
+                </main>
+            </div>
+        );
+    }
+}
+
+export default Login;
--- src/app/index.js
+++ src/app/index.js
@@ -9,6 +9,7 @@ import AmpliacionLugar from './components/AmpliacionLugar';
 import FormularioQuejas from './components/FormularioQuejas';
 import Principal from './components/Principal';
 import PerfilUsuario from './components/PerfilUsuario';
+import Login from './components/Login';
 
 const btnPrincipal = document.getElementById('btn-ppal');
 const btnRegistroTurista = document.getElementById('btn-registroTurista');
@@ -18,13 +19,13 @@ const btnQueja = document.getElementById('btn-queja');
 const btnExtremo = document.getElementById('btn-extremo');
 const btnAmpliar = document.getElementById('btn-ampliar');
 
-render(<Principal />, document.getElementById('app'));
+render(<Login />, document.getElementById('app'));
 //Acciones de los botones
-btnPrincipal.addEventListener("click", () => { render(<Principal />, document.getElementById('app')); });
+/*btnPrincipal.addEventListener("click", () => { render(<Principal />, document.getElementById('app')); });
 btnRegistroTurista.addEventListener("click", () => { render(<RegistroTurista />, document.getElementById('app')); });
 btnregistroEntidad.addEventListener("click", () => { render(<RegistroEntidad />, document.getElementById('app')); });
 btnPrecios.addEventListener("click", () => { render(<Precios />, document.getElementById('app')); });
 btnQueja.addEventListener("click", () => { render(<FormularioQuejas />, document.getElementById('app')); });
 btnExtremo.addEventListener("click", () => { render(<RutasExtremas />, document.getElementById('app')); });
-btnAmpliar.addEventListener("click", () => { render(<AmpliacionLugar />, document.getElementById('app')); });
+btnAmpliar.addEventListener("click", () => { render(<AmpliacionLugar />, document.getElementById('app')); });*/
 
